0052cbe472f5374f60b7c5445f674252a013eb6a
omk963 <132976939+omk963@users.noreply.github.com>
2024-07-14 22:09:26 -0700

    feat(app): add middleware, implement people API, refactor with routes and controllers

--- 02-express-tutorial/app.js
+++ 02-express-tutorial/app.js
@@ -1,48 +1,79 @@
 const express = require('express');
 const app = express();
-const { products } = require('./data');
+const { products, people } = require('./data');
+const peopleRouter = require('./routes and controllers/routes/people');
+
+const logger = (req, res, next) => {
+  const method = req.method
+  const url = req.url
+  const time = new Date().getFullYear()
+  console.log(method, url, time)
+  next()
+}
+
+app.use(logger)
 
 app.use(express.static('./public'));
 
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
+app.use("/api/v1/people", peopleRouter);
+
 app.get('/api/v1/test', (req, res) => {
-    res.json({ message: 'It Worked!' });
+  res.json({ message: 'It Worked!' });
 })
 
+// app.get('/api/v1/people', (req, res) => {
+//   res.json(people);
+// })
+
+// app.post('/api/v1/people', (req, res) => {
+//   const { name } = req.body;
+//   if (name) {
+//     people.push({ id: people.length + 1, name });
+//     res.status(201).json({ success: true, name });
+//   }
+//   else {
+//     res.status(400).json({ success: false, message: "Please provide a name" });
+//   }
+// })
+
 app.get('/api/v1/products', (req, res) => {
-    res.json(products);
+  res.json(products);
 })
 
 app.get('/api/v1/products/:productID', (req, res) => {
-    const idToFind = parseInt(req.params.productID);
-    const product = products.find((p) => p.id === idToFind);
-    if(!product) {
-        res.status(404).json({message: 'That product was not found.'});
-    }
-    res.status(200).json(product);
+  const idToFind = parseInt(req.params.productID);
+  const product = products.find((p) => p.id === idToFind);
+  if (!product) {
+    res.status(404).json({ message: 'That product was not found.' });
+  }
+  res.status(200).json(product);
 })
 
 app.get('/api/v1/query', (req, res) => {
-    const { search, limit } = req.query
-    let sortedProducts = [...products]
-  
-    if (search) {
-      sortedProducts = sortedProducts.filter((product) => {
-        return product.name.startsWith(search)
-      })
-    }
-    if (limit) {
-      sortedProducts = sortedProducts.slice(0, Number(limit))
-    }
-    if (sortedProducts.length < 1) {
-      return res.status(200).json({ sucess: true, data: [] })
-    }
-    res.status(200).json(sortedProducts)
-  })
+  const { search, limit } = req.query
+  let sortedProducts = [...products]
+
+  if (search) {
+    sortedProducts = sortedProducts.filter((product) => {
+      return product.name.startsWith(search)
+    })
+  }
+  if (limit) {
+    sortedProducts = sortedProducts.slice(0, Number(limit))
+  }
+  if (sortedProducts.length < 1) {
+    return res.status(200).json({ sucess: true, data: [] })
+  }
+  res.status(200).json(sortedProducts)
+})
 
 app.all('*', (req, res) => {
-    res.status(404).send('resource not found')
+  res.status(404).send('resource not found')
 })
 
 app.listen(3000, () => {
-    console.log('server is listening on port 3000....')
+  console.log('server is listening on port 3000....')
 })
